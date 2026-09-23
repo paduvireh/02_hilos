1859ec2d332c22a24e8cc2c8f1347bb70c70634c
paduvireh <paduvireh@unjbg.edu.pe>
2026-09-23 10:17:49 -0500

    03. Sincronización con mutex

--- hilos.c
+++ hilos.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void* hilo_funcion(void* arg) {
+pthread_mutex_t mutex;
+
+void* hilo_funcion_con_mutex(void* arg) {
+	pthread_mutex_lock(&mutex);
 	printf("Hola desde el hilo %ld\n", (long)arg);
+	pthread_mutex_unlock(&mutex);
 	pthread_exit(NULL);
 }
 
 int main() {
 	pthread_t hilo1, hilo2;
 
-	pthread_create(&hilo1, NULL, hilo_funcion, (void*)1);
-	pthread_create(&hilo2, NULL, hilo_funcion, (void*)2);
+	pthread_mutex_init(&mutex, NULL);
+
+	pthread_create(&hilo1, NULL, hilo_funcion_con_mutex, (void*)1);
+	pthread_create(&hilo2, NULL, hilo_funcion_con_mutex, (void*)2);
 
 	pthread_join(hilo1, NULL);
 	pthread_join(hilo2, NULL);
+
+	pthread_mutex_destroy(&mutex);
 
 	printf("Finalizó la ejecución del programa principal\n");
 	return 0;
